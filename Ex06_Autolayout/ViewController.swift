ebb3a90a5d8c642f40b93aff7e600f543a80bae9
SongYunGeun <rjawoakstp@naver.com>
2022-01-03 17:05:33 +0900

    오토 레이아웃 연습

--- Ex06_Autolayout/ViewController.swift
+++ Ex06_Autolayout/ViewController.swift
@@ -5,13 +5,33 @@
 //  Created by 송윤근 on 2022/01/03.
 //
 
+//화면 배치 레이아웃 방법 2가지
+// 1. 오토 리사이징 : 기본값, 간단한 배치 용도.
+// 2. 오토 레이아웃 : 주로 사용됨.
+// 사용용도 : 다양한 화면 해상도에 대응.
+// xcode 11 부터는 swift Ui 지원- iOS 13. 부터 사용이 가능하다.
+// 스위프트 ui가 좋긴 하지만 이후 3~5년뒤쯤 상용화 될 듯.
+
+//앱을 만들 때 해상도를 어디까지 지원해야 하는가.
+//모든 화면을 지원하기에는 개인, 소규모 개발자에겐 무리(시간과 노력이 많이 들어감)
+//1. 아이패드 해상도는 PC해상도라고 생각해야 한다.(협상을 해야한다.)그래서 제외한다.
+//2. 너무 작은 해상도는 지원 불가. 예)아이폰 4, 5S
+//3. 지원 하는것은 : 최신 아이폰. ~ 5년전 모델 정도.
+//ios 지원 역시 한 11~12 정도?
+//진짜 한 2~3년 뒤면 swift ui 많이 사용할듯.
+
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
